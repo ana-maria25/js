65fa1bdf4c55751f29f1413cb6a5ed57dc615803
Ani <alderete.ana92@gmail.com>
2025-11-18 19:10:10 -0300

    agrego logica2.js

--- practicasLogica/logica2.js
+++ practicasLogica/logica2.js
@@ -16,3 +16,86 @@
 // Usar variables num1, operador, num2
 // Al finalizar, limpiar las variables
 // Si falta algo mostrar: “Operación incompleta”
+
+//variable globales gurdan estado de operacion
+let num1 = null;
+let operador = null;
+let num2 = null;
+
+//elemento donde mostramos resultado
+const pantalla = document.getElementById("pantalla");
+
+//seleccionamos todos los botones del HTML
+const botones = document.querySelectorAll("button");
+
+// ***AGREGAMOS EL EVENTO A CADA BOTON
+// forEach = repetir un bloque de codigo para cada boton***
+
+botones.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    //obtenemos el texto del boton que se clickeo
+    //("10","20","+","-"..)
+    const valor = boton.textContent;
+
+    //1-PRIMERO PREGUNTAMOS: EL BOTON ES UN NUMERO?
+
+    if (!isNaN(valor)) {
+      // si todavia no tenemos num1 --> este numero es num1
+      if (num1 === null) {
+        num1 = Number(valor); //convertimos texto en numero
+        pantalla.textContent = `Operacion: ${num1}`;
+        return; //salimos. no seguimos ejecutando
+      }
+
+      //si ya tenemos num1 pero NO tenemos operador aun--> ERROR
+
+      if (operador === null) {
+        pantalla.textContent = "Operacion invalida (falta operador)";
+        num1 = null;
+        operador = null;
+        num2 = null;
+        return;
+      }
+
+      //si llegamos aca-->ya hay num1 y operador->este es num2
+      num2 = Number(valor);
+      pantalla.textContent = `Operación: ${num1} ${operador} ${num2}`;
+      return;
+    }
+
+    // si es operador (+o-)
+    if (valor === "+" || valor === "-") {
+      if (num1 !== null && operador === null) {
+        operador = valor; //guardamos valor
+        pantalla.textContent = `Operacion: ${num1} ${operador}`;
+      } else {
+        pantalla.textContent = "Operacion invalida";
+        num1 = null;
+        operador = null;
+        num2 = null;
+      }
+
+      return;
+    }
+
+    //si apreto resultado
+    if (valor === "Resultado") {
+      if (num1 === null || operador === null || num2 === null) {
+        pantalla.textContent = `Operacion incompleta`;
+        return;
+      }
+
+      function evaluar(num1, operador, num2) {
+        if (operador === "+") return num1 + num2;
+        if (operador === "-") return num1 - num2;
+      }
+
+      let rta = evaluar(num1, operador, num2);
+
+      pantalla.textContent = `Operación: ${num1} ${operador} ${num2} = ${rta}`;
+      num1 = null;
+      operador = null;
+      num2 = null;
+    }
+  });
+});
